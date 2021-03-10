941b0bd633bec19bb548ddd7a85e808be9f1c6af
unknown <violet1395@naver.com>
2021-03-10 19:21:11 +0900

    Initial commit 2021.3.10

---
+++ app/src/main/java/com/example/sockdack_app/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.sockdack_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Home home;
+    private MyPage mypage;
+    private CheckList checklist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.checklist:
+                        setFrag(0);
+                        break;
+                    case R.id.home:
+                        setFrag(1);
+                        break;
+                    case R.id.mypage:
+                        setFrag(2);
+                        break;
+                }
+                return true;
+            }
+        });
+        home = new Home();
+        mypage = new MyPage();
+        checklist = new CheckList();
+        setFrag(1); //첫 프래그먼트 화면을 무엇으로 지정해줄 것인지 선택
+
+    }
+
+    //프레그먼트 교체가 일어나는 실행문이다.
+    private void setFrag(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction(); //실제적인 프레그먼트 교체에서 사용
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame,checklist);
+                ft.commit(); //저장의미
+                break;
+            case 1:
+                ft.replace(R.id.main_frame,home);
+                ft.commit(); //저장의미
+                break;
+            case 2:
+                ft.replace(R.id.main_frame,mypage);
+                ft.commit(); //저장의미
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sockdack_app/MyPage.java
@@ -0,0 +1,21 @@
+package com.example.sockdack_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class MyPage extends Fragment {
+
+    private View view;
+
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_page, container, false);
+    }
+}

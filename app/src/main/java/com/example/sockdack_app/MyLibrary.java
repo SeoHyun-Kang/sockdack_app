5835ac8c36daa0d37bf1e3444ef8caceac9a5e73
SeoHyun-Kang <violet1395@naver.com>
2021-03-21 20:20:56 +0900

    0321 수정본

--- app/src/main/java/com/example/sockdack_app/MyLibrary.java
+++ app/src/main/java/com/example/sockdack_app/MyLibrary.java
@@ -2,26 +2,20 @@ package com.example.sockdack_app;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-/*
+/**
  * A simple {@link Fragment} subclass.
- * Use the {@link MyPage#newInstance} factory method to
+ * Use the {@link MyLibrary#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MyPage extends Fragment {
+public class MyLibrary extends Fragment {
 
-    /*// TODO: Rename parameter arguments, choose names that match
+    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -30,7 +24,7 @@ public class MyPage extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public MyPage() {
+    public MyLibrary() {
         // Required empty public constructor
     }
 
@@ -40,12 +34,11 @@ public class MyPage extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
+     * @return A new instance of fragment MyLibrary.
      */
     // TODO: Rename and change types and number of parameters
-    /*
-    public static MyPage newInstance(String param1, String param2) {
-        MyPage fragment = new MyPage();
+    public static MyLibrary newInstance(String param1, String param2) {
+        MyLibrary fragment = new MyLibrary();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -56,33 +49,16 @@ public class MyPage extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }*/
-
-
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false);
+        return inflater.inflate(R.layout.fragment_my_library, container, false);
     }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
-        super.onViewCreated(view,savedInstanceState);
-        view.findViewById(R.id.my1).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                NavHostFragment.findNavController(MyPage.this).navigate(R.id.action_mypage_to_myLibrary);
-            }
-        });
-    }
-    /*public void replaceFragment(Fragment fragment){
-        FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_nav_main,fragment).addToBackStack(null).commit();
-    }
-    */
 }
